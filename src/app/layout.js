ecc7623ecb467d68b1c0143076c5e33eb87df4a7
santy <santy@DESKTOP-7KIGB8O>
2023-12-27 18:39:26 -0500

    added clerk middleware to restrict access to certain pages to authenticated users only

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,9 @@ export default function RootLayout({
         <ClerkProvider>
             <html lang="en">
                 <body>
-                    <BootstrapClient />
                     <Navbar />
                     {children}
+                    <BootstrapClient />
                 </body>
             </html>
         </ClerkProvider>
