15f5b8172b77573f812ab483a4286811af902b6c
santy <santy@DESKTOP-7KIGB8O>
2023-12-27 18:39:26 -0500

    added clerk middleware to restrict access to certain pages to authenticated users only

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import './css/style.css'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import BootstrapClient from '@/components/Bootstrap/BootstrapClient';
-import Navbar from '@/components/navbar';
 
 
 export const metadata = {
@@ -18,7 +17,6 @@ export default function RootLayout({
         <ClerkProvider>
             <html lang="en">
                 <body>
-                    <Navbar />
                     {children}
                     <BootstrapClient />
                 </body>
