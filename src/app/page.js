c411d734e395bffebb8a11230e1bed41cf3f029e
geekynice <nicebanjaraa@gmail.com>
2023-12-27 23:40:44 -0500

    featured_section_added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Hero from "@/components/hero/hero";
 import Navbar from "@/components/navbar/navbar";
 import Process from "@/components/process/process";
 import About from "@/components/about/about";
+import FeaturedPets from "@/components/featured/featuredPets";
 
 export default function Home() {
     // make the first div background color red with bootstrap classes
@@ -11,6 +12,7 @@ export default function Home() {
             <Hero />
             <Process />
             <About />
+            <FeaturedPets/>
         </>
     )
 }
