2a70e9f563e34d21c5c0d01dcd1ba5703da6e0dc
santy <santy@DESKTOP-7KIGB8O>
2023-12-27 20:13:05 -0500

    rebased with main

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({
             <html lang="en">
                 <body>
                     {children}
+                    <BootstrapClient />
                 </body>
             </html>
         </ClerkProvider>
