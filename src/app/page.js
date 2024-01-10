201590a7b368b303da82282b961c611cb6373cd2
santy <santy@DESKTOP-7KIGB8O>
2024-01-10 13:36:04 -0500

    fixed merge conflicxts

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,10 @@ import Navbar from "@/components/navbar/navbar";
 import Process from "@/components/process/process";
 import About from "@/components/about/about";
 import FeaturedPets from "@/components/featured/featuredPets";
-<<<<<<< HEAD
-=======
 import Testimonials from "@/components/testimonials/testimonials";
 import Partners from "@/components/partners/partners";
 import CTA from "@/components/cta/callToAction";
 import Footer from "@/components/footer/footer";
-
->>>>>>> 7e325867fa9cde3d51035d8ed1d0fb5b5fffedcd
 
 export default function Home() {
     // make the first div background color red with bootstrap classes
@@ -20,14 +16,11 @@ export default function Home() {
             <Hero />
             <Process />
             <About />
-            <FeaturedPets/>
-<<<<<<< HEAD
-=======
-            <Testimonials/>
-            <Partners/>
-            <CTA/>
-            <Footer/>
->>>>>>> 7e325867fa9cde3d51035d8ed1d0fb5b5fffedcd
+            <FeaturedPets />
+            <Testimonials />
+            <Partners />
+            <CTA />
+            <Footer />
         </>
     )
 }
